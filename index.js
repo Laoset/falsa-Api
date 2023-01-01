295789bd4f01be71f141cf3465c8ceb3a2bea596
Laoset <xlaospt2@gmail.com>
2023-01-01 18:54:51 -0300

    pasedtoVercel

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+// Middlewares
+const app = express();
+app.use(express.json());
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+// Routes
+app.use("/home", home);
 
-const PORT = 3000
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const jsonServer = require('json-server')
+// const cors = require('cors')
+// const path = require('path')
+
+// const server = jsonServer.create()
+// const router = jsonServer.router(path.join(__dirname, 'db.json'))
+// const middlewares = jsonServer.defaults()
+
+// server.use(cors())
+// server.use(jsonServer.bodyParser)
+// server.use(middlewares)
+// server.use(router)
+
+// const PORT = 3000
+
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on http://localhost:${PORT}`)
+// })
